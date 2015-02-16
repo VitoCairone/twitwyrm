1726fdb44ed9f1bdd6ad1943e69be558d344c4cc
Rob Green <rob.green@zefr.com>
2015-02-15 18:09:28 -0800

    Extract youtube ids

--- main.go
+++ main.go
@@ -2,22 +2,49 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 
 	"github.com/ChimeraCoder/anaconda"
 )
 
 func main() {
+	http.HandleFunc("/tweets", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	anaconda.SetConsumerKey("tDAaN4yvVg5wFVlfkxeEUOzHm")
 	anaconda.SetConsumerSecret("fRtI78PU9T67xNM5dylEO8146qEjtF5qVEHHZIBLuoJ6BHHjm0")
 	api := anaconda.NewTwitterApi("332989840-mBSNofbvaHW2eyENXlAmI52GLpyGQwVvRav5jdsl",
 		"y6N3Ty0d99JDARs4FPvHmYjtocUrpQlRcN2uQ9ljTWeBu")
 
 	values := url.Values{}
+	params := r.URL.Query()
 	values.Add("track", "youtube com,youtu be")
-	stream := api.PublicStreamFilter(values)
+	stream := api.PublicStreamFilter(params)
+
 	for item := range stream.C {
-		fmt.Println(item)
+		tweet, ok := item.(anaconda.Tweet)
+		if ok {
+			urls := tweet.Entities.Urls
+			for _, u := range urls {
+				parsed, err := url.Parse(u.Expanded_url)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+
+				if videoId, ok := parsed.Query()["v"]; ok {
+					fmt.Println(videoId[0])
+
+					w.Write([]byte(videoId[0]))
+				}
+			}
+		} else {
+			fmt.Println("not a tweet")
+			fmt.Println(item)
+		}
 
 	}
 }

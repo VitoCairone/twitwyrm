d5f58de7efb3e17af087144225274da44e5de196
Rob Green <rob.green@zefr.com>
2015-02-15 19:27:06 -0800

    Write to mongo in a seperate goroutine

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/mgo.v2"
-	_ "gopkg.in/mgo.v2/bson"
 	"net/http"
 	"net/url"
 	"time"
 
 	"github.com/ChimeraCoder/anaconda"
+	"gopkg.in/mgo.v2"
 )
 
 type VideoOccurence struct {
@@ -17,11 +16,7 @@ type VideoOccurence struct {
 }
 
 func main() {
-	http.HandleFunc("/tweets", handler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func findVideos() {
+	voChan := findVideos()
 	session, err := mgo.Dial("localhost:27017")
 	if err != nil {
 		panic(err)
@@ -31,63 +26,71 @@ func findVideos() {
 	c := session.DB("twitwyrm").C("VideoOccurences")
 	session.SetMode(mgo.Monotonic, true)
 
-	anaconda.SetConsumerKey("tDAaN4yvVg5wFVlfkxeEUOzHm")
-	anaconda.SetConsumerSecret("fRtI78PU9T67xNM5dylEO8146qEjtF5qVEHHZIBLuoJ6BHHjm0")
-	api := anaconda.NewTwitterApi("332989840-mBSNofbvaHW2eyENXlAmI52GLpyGQwVvRav5jdsl",
-		"y6N3Ty0d99JDARs4FPvHmYjtocUrpQlRcN2uQ9ljTWeBu")
+	for vo := range voChan {
+		err = c.Insert(vo)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 
-	values := url.Values{}
-	// params := r.URL.Query()
-	values.Add("track", "youtube com,youtu be")
-	stream := api.PublicStreamFilter(values)
+	http.HandleFunc("/tweets", handler)
+	http.ListenAndServe(":8080", nil)
+}
 
-	for item := range stream.C {
-		tweet, ok := item.(anaconda.Tweet)
-		if ok {
-			urls := tweet.Entities.Urls
-			for _, u := range urls {
-				parsed, err := url.Parse(u.Expanded_url)
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
+func findVideos() chan VideoOccurence {
+	voChan := make(chan VideoOccurence)
 
-				if videoId, ok := parsed.Query()["v"]; ok {
-					fmt.Println(videoId[0])
+	go func() {
+		anaconda.SetConsumerKey("tDAaN4yvVg5wFVlfkxeEUOzHm")
+		anaconda.SetConsumerSecret("fRtI78PU9T67xNM5dylEO8146qEjtF5qVEHHZIBLuoJ6BHHjm0")
+		api := anaconda.NewTwitterApi("332989840-mBSNofbvaHW2eyENXlAmI52GLpyGQwVvRav5jdsl",
+			"y6N3Ty0d99JDARs4FPvHmYjtocUrpQlRcN2uQ9ljTWeBu")
 
-					// w.Write([]byte(videoId[0]))
+		values := url.Values{}
+		values.Add("track", "youtube com,youtu be")
+		stream := api.PublicStreamFilter(values)
 
-					vo := &VideoOccurence{
-						YTID:      videoId[0],
-						Timestamp: time.Now(),
-					}
-
-					err = c.Insert(vo)
+		for item := range stream.C {
+			tweet, ok := item.(anaconda.Tweet)
+			if ok {
+				urls := tweet.Entities.Urls
+				for _, u := range urls {
+					parsed, err := url.Parse(u.Expanded_url)
 					if err != nil {
 						fmt.Println(err)
 						continue
 					}
-					// result := VideoOccurence{}
-					// err = c.Find(bson.M{"id": videoId[0]}).One(&result)
-					// if err != nil {
-					// 	fmt.Println(err)
-					// 	fmt.Println("find")
-					// 	continue
-					// }
-					//
-					// fmt.Println("VO:", result)
-					// return
+					vo := VideoOccurence{}
+					switch parsed.Host {
+					case "youtu.be":
+						videoId := parsed.Path
 
-					// err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-					// 	&Person{"Cla", "+55 53 8402 8510"})
+						if len(videoId) == 11 { // All youtube IDs are length 11
+							vo.YTID = videoId
+							vo.Timestamp = time.Now()
+							voChan <- vo
+						}
+					case "youtube.com":
+
+						if videoIdSlice, ok := parsed.Query()["v"]; ok {
+							videoId := videoIdSlice[0]
+							if len(videoId) == 11 { // All youtube IDs are length 11
+								vo.YTID = videoId
+								vo.Timestamp = time.Now()
+								voChan <- vo
+							}
+						}
+
+					}
 				}
 			}
-		} else {
-			fmt.Println("not a tweet")
-			fmt.Println(item)
 		}
+	}()
+	return voChan
+}
 
-	}
+func writeResults() {
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
